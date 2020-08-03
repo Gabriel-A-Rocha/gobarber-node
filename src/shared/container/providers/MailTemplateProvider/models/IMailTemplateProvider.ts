b9b4efe282198b8447cc26b1995cc87d5ed91bdc
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-08-03 12:08:05 -0300

    Install and set handlebars for email templating

--- src/shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider.ts
@@ -1,6 +1,7 @@
 import IParseMailtemplateDTO from '../dtos/IParseMailtemplateDTO';
 
 interface IMailTemplateProvider {
+  // assembles the email with the provided info
   parse(data: IParseMailtemplateDTO): Promise<string>;
 }
 
