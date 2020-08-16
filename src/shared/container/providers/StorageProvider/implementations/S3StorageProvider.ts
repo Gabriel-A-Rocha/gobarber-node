37b89875987e658dc57dcd60b89a006dde90cb49
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-08-15 23:52:20 -0300

    Implement S3 integration for storage on the cloud

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import aws, { S3 } from 'aws-sdk';
+
+import uploadConfig from '@config/upload';
+
+import IStorageProvider from '../models/IStorageProvider';
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: 'sa-east-1',
+    });
+  }
+
+  // receives the file path as argument
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalPath, {
+      encoding: 'utf-8',
+    });
+
+    await this.client
+      .putObject({
+        Bucket: 'app-gobarber-gabrielrocha',
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+      })
+      .promise();
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(uploadConfig.uploadsFolder, file);
+
+    // return in case file does not exist
+    try {
+      await fs.promises.stat(filePath);
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
+
+export default S3StorageProvider;
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -2,12 +2,13 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from '../StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '../StorageProvider/implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  providers.disk,
-);
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.s3);
+
+new S3StorageProvider();
